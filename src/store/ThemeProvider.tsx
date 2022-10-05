08cf701d37b9989e9664272000756ada14305682
majeed <majeed.dl@gmail.com>
2022-10-05 22:36:34 +0300

    add ThemeProvider

---
+++ src/store/ThemeProvider.tsx
@@ -0,0 +1,29 @@
+import { createContext, FunctionComponent, ReactElement, useState } from "react";
+
+
+export const ThemeContext = createContext({
+    dir : 'rtl',
+    SetThemeDir : (_dir:string)=> {}
+});
+
+type ThemeProviderType = {
+    children : ReactElement
+}
+
+export const ThemeProvider : FunctionComponent<ThemeProviderType> = ({ children}) =>{
+
+    const [ dir , setDir ] = useState('rtl')
+
+    const SetThemeDir = (_dir:string)=>{
+        setDir(_dir)
+    }
+    
+    return (
+        <ThemeContext.Provider value={{
+            dir,
+            SetThemeDir
+        }} >
+            {children}
+        </ThemeContext.Provider>
+    )
+}
