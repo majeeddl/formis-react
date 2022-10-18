e37acac0f92b549c78ee4619f1be91fab787e995
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-10-18 17:50:07 +0300

    use card for form editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+
+
 import "./App.css";
 
 import { Formis, FormisModeler } from "./lib/index";
--- src/lib/FormisModeler.tsx
+++ src/lib/FormisModeler.tsx
@@ -1,18 +1,24 @@
-
-
-import { Grid } from '@mantine/core';
-import React from 'react'
+import { Card, Grid } from "@mantine/core";
+import React from "react";
 
 const FormModeler = () => {
   return (
     <>
       FormModeler
       <Grid columns={24}>
-        <Grid.Col span={4}>1</Grid.Col>
-        <Grid.Col span={20}>2</Grid.Col>
+        <Grid.Col span={4}>
+          <Card shadow="sm" p="lg" radius="md" withBorder>
+            <div>salam</div>
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={20}>
+          <Card shadow="sm" p="lg" radius="md" withBorder bo>
+            <div>salam</div>
+          </Card>
+        </Grid.Col>
       </Grid>
     </>
   );
-}
+};
 
-export default FormModeler
+export default FormModeler;
