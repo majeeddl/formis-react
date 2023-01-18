3f01d3040585ba3b828ee1413e356a9b9298883b
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-01-18 14:06:32 +0300

    add button component

--- src/components/form/FormItem.tsx
+++ src/components/form/FormItem.tsx
@@ -1,5 +1,4 @@
 import { Switch } from "@mantine/core";
-import React, { FunctionComponent } from "react";
 import Input from "./formItems/Input";
 import Divider from "./formItems/layouts/Divider";
 import Grid from "./formItems/layouts/Grid";
@@ -8,6 +7,7 @@ import Checkbox from "./formItems/Checkbox";
 import NumberInput from "./formItems/NumberInput";
 import Select from "./formItems/Select";
 import Textarea from "./formItems/Textarea";
+import Button from "./formItems/Button";
 
 export enum FormItemStateEnum {
   View = "view",
@@ -27,6 +27,15 @@ export enum FormItemTypeEnum {
   Switch = "switch",
   Select = "select",
   MultiSelect = "multiSelect",
+  Button = "button",
+}
+
+export enum FormItemSizeEnum {
+  xs = "xs",
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xl = "xl",
 }
 
 export type FormItemProps = {
@@ -34,7 +43,11 @@ export type FormItemProps = {
   type: FormItemTypeEnum;
 };
 
-const FormItem = ({ state = FormItemStateEnum.View, type, ...props }: FormItemProps) => {
+const FormItem = ({
+  state = FormItemStateEnum.View,
+  type,
+  ...props
+}: FormItemProps) => {
   return (
     <>
       <div className="form-item-box">
@@ -49,6 +62,7 @@ const FormItem = ({ state = FormItemStateEnum.View, type, ...props }: FormItemPr
         {type == FormItemTypeEnum.Checkbox && <Checkbox {...props}></Checkbox>}
         {type == FormItemTypeEnum.Switch && <Switch {...props}></Switch>}
         {type == FormItemTypeEnum.Select && <Select {...props}></Select>}
+        {type == FormItemTypeEnum.Button && <Button {...props}></Button>}
       </div>
     </>
   );
