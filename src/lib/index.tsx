94dad4fb58852bc0a2c2fe0b06635d611cfa6e56
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2023-01-27 16:36:45 +0300

    update package and fixing building configuration

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,5 +1,7 @@
 import "./index.css";
 
-export { default as FormisViewer } from "./FormisViewer";
-export { default as FormisModeler } from "./FormisModeler";
-export { default as Formis } from "./Formis";
+import FormisViewer from "./FormisViewer";
+import FormisModeler from "./FormisModeler";
+import Formis from "./Formis";
+
+export { Formis, FormisModeler, FormisViewer };
