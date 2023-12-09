eb9d8591ff796d163febf9539d4aabd2759784ff
Majeed Dourandeesh <majeed.dl@gmail.com>
2023-12-09 23:23:40 +0300

    feat : number input refactoring has been done

--- src/lib/components/form/formItems/NumberInput.tsx
+++ src/lib/components/form/formItems/NumberInput.tsx
@@ -1,23 +1,54 @@
-import React from "react";
-import { NumberInput as NumberInputMantine } from "@mantine/core";
+import React, { useState } from "react";
+import { NumberInput as NumberInputMantine, NumberInputProps } from "@mantine/core";
+import { useFormisDispatchContext } from "../../../context/formis.context";
+
+export type TNumberInputProps = NumberInputProps & {
+  id: string;
+  value: any;
+  dirty: boolean;
+};
+
 const NumberInput = ({
+  id,
   label = "number input",
   description = "",
   placeholder = "",
   required = false,
+  disabled = false,
   value = null,
   error = "",
-  onChange = (e:any) => {
-    console.log(e);
-  },
-}) => {
-  //  const [chekcboxValue, setChekcboxValue] = useState(value);
+  dirty = false,
+  ...props
+}: TNumberInputProps) => {
+  const { dispatchItems } = useFormisDispatchContext();
+  const [numberValue, setNumberValue] = useState(value);
 
   const customAttr: any = {};
 
   if (required) {
     customAttr["withAsterisk"] = true;
+    if (typeof numberValue != "number" && dirty) error = "This field is required";
   }
+
+  const handleChange = (_value: any) => {
+    let _error = "";
+
+    if (required && _value == null) {
+      _error = "This field is required";
+    }
+
+    dispatchItems({
+      type: "update",
+      payload: {
+        id,
+        value: _value,
+        error: _error,
+        dirty: true,
+      },
+    });
+
+    setNumberValue(_value);
+  };
 
   return (
     <>
@@ -25,10 +56,12 @@ const NumberInput = ({
         label={label}
         description={description}
         placeholder={placeholder}
-        // value={value}
-        onChange={onChange}
+        disabled={disabled}
+        value={numberValue}
+        onChange={handleChange}
         error={error}
         {...customAttr}
+        {...props}
       ></NumberInputMantine>
     </>
   );
